de3296d6436c54ea85225c71502a3238efeee581
Enzo Bozzani <bozzanienzo@gmail.com>
2023-04-18 23:13:21 -0300

    inicialized creation of home page

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+				compress: true,
+				port: 80,
+    },
+    entry: {
+        index: './src/index.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                {loader: 'style-loader'},
+                {loader: 'css-loader'},
+                {loader: 'postcss-loader', 
+                    options: {
+                        postcssOptions: {
+                            plugins: () => [
+                                autoprefixer
+                            ]
+                        }
+                    }
+                },
+                {loader: 'sass-loader'}
+            ]
+        }, {
+            test: /\.js$/,
+            use: ['babel-loader']
+        }]
+    },
+    output: {
+        filename: '[name].min.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './dist/index.html' })
+    ]
+}
